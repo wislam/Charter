ac82955be042a0496941ee95784a9028ee41c4fa
Usama Bin Shafqat <usamashafqat@gmail.com>
2017-04-06 01:15:04 -0400

    Database workssgit commit (sorry about debuggin files i'll figure out how to eliminate them next time

--- index.ios.js
+++ index.ios.js
@@ -1,63 +1,97 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+* Sample React Native App
+* https://github.com/facebook/react-native
+* @flow
+*/
 
 import * as firebase from 'firebase';
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+	AppRegistry,
+	StyleSheet,
+	Text,
+	ListView,
+	View
 } from 'react-native';
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyDYEq76HH9bRI1YbhV-Q8_-B5ue7Vyh4-g",
-  authDomain: "charter-2d47b.firebaseapp.com",
-  databaseURL: "https://charter-2d47b.firebaseio.com",
-  storageBucket: "charter-2d47b.appspot.com",
+	apiKey: "AIzaSyDYEq76HH9bRI1YbhV-Q8_-B5ue7Vyh4-g",
+	authDomain: "charter-2d47b.firebaseapp.com",
+	databaseURL: "https://charter-2d47b.firebaseio.com",
+	storageBucket: "charter-2d47b.appspot.com",
 };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 export default class Charter extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to Charter!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			dataSource: new ListView.DataSource({
+				rowHasChanged: (row1, row2) => row1 !== row2,
+			})
+		};
+		this.chartersRef = this.getRef().child('charters');
+
+	}
+
+	getRef() {
+		console.log(firebaseApp.database().ref());
+		return firebaseApp.database().ref();
+	}
+
+	listenForCharters(chartersRef) {
+		chartersRef.on('value', (snap) => {
+
+			// get children as an array
+			var charters = [];
+			snap.forEach((child) => {
+				charters.push(child.val().destination);
+			});
+
+			this.setState({
+				dataSource: this.state.dataSource.cloneWithRows(charters)
+			});
+
+		});
+	}
+
+	componentDidMount() {
+		this.listenForCharters(this.chartersRef);
+	}
+
+	render() {
+		console.log(this.state.dataSource)
+		return (
+			<View style={{flex: 1, paddingTop: 22}}>
+			<Text>DATABASE: </Text>
+			<ListView
+			dataSource={this.state.dataSource}
+			renderRow={(rowData) => <Text>{rowData}</Text>}
+			/>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF',
+	},
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10,
+	},
+	instructions: {
+		textAlign: 'center',
+		color: '#333333',
+		marginBottom: 5,
+	},
 });
 
 AppRegistry.registerComponent('Charter', () => Charter);
