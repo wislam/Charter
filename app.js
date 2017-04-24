2ca39f666d698707fd2b68f6667f5215bb40be0f
mattrosen <mcr3@princeton.edu>
2017-04-24 17:44:28 -0400

    Search + UI

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ import {
 	StyleSheet,
 	DatePickerIOS,
 	TouchableHighlight,
-	ListView
+	ListView,
+	Platform
 } from 'react-native';
 import { Grid, Col, Row, Container, Header, Content, Form, Item, Input, Label, Left, Right, Body, Icon, Title, InputGroup, List, ListItem } from 'native-base';
 import {
@@ -33,7 +34,20 @@ var currentUser = 'dudebro';
 
 class LoginScreen extends React.Component {
 	static navigationOptions = {
-		title: "Charter"
+		title: ' ',
+		header: {
+		    titleStyle: {
+		     	color: '#B5BABF',
+		     	letterSpacing: 2,
+		     	fontWeight: '500'
+		    },
+		    style: {
+		     	backgroundColor: '#092742'
+		    },
+		    tintColor: {
+		      	backgroundColor: '#FCEE6D'
+		    }
+		  }
 	};
 	render() {
 		const { navigate } = this.props.navigation;
@@ -45,35 +59,47 @@ class LoginScreen extends React.Component {
 			title="Log in"
 			/>
 			</View>*/
-			<Container backgroundColor='#EFF0F0'>
-			<Content style={{ width: '80%', marginLeft: '9%'}}>
-			<Form >
-			<Item floatingLabel>
-			<Label>Username</Label>
-			<Input />
-			</Item>
-			<Item floatingLabel >
-			<Label>Password</Label>
-			<Input secureTextEntry={true}/>
-			</Item>
-			</Form>
-			</Content>
-			<Button
-			onPress={() => navigate('Search', { user: 'Christopher Eisgruber' })}
-			title='LOG IN' fontweight='bold' backgroundColor='#7CD3C8'/>
-			</Container>
+			<View style={styles.container}>
+				<Text style={{color:"white", fontSize:65, textAlign:"center", letterSpacing:2.5, fontWeight:'800', fontFamily:"Jaapokki subtract"}}>CHARTER</Text>
+				<Content style={{ width: '80%', marginLeft: '9%'}}>
+					<Form >
+						<Item floatingLabel>
+							<Label style={{color:"#DDDDDD"}}>Username</Label>
+							<Input style={{color:"#DDDDDD"}} />
+						</Item>
+						<Item floatingLabel>
+							<Label style={{color:"#DDDDDD"}}>Password</Label>
+							<Input secureTextEntry={true} style={{color:"#DDDDDD"}}/>
+						</Item>
+					</Form>
+				</Content>
+				<TouchableHighlight style={styles.button} onPress={() => navigate('Search', { })} underlayColor='#e2d662'>
+					<Text style={styles.buttonText}>LOG IN</Text>
+				</TouchableHighlight>
+			</View>
 		);
 	}
 }
 
-
-
-//////////////////
+///
 
 class SearchScreen extends React.Component {
 	static navigationOptions = {
 		// Nav options can be defined as a function of the navigation prop:
-		title: ({ state }) => `Search`,
+		title: ({ state }) => ` `,
+		header: {
+		    titleStyle: {
+		     	color: '#B5BABF',
+		     	letterSpacing: 2,
+		     	fontWeight: '500'
+		    },
+		    style: {
+		     	backgroundColor: '#092742'
+		    },
+		    tintColor: {
+		      	backgroundColor: '#FCEE6D'
+		    }
+		  }
 	};
 
 	constructor(props) {
@@ -81,13 +107,14 @@ class SearchScreen extends React.Component {
 		this.state = {
 			dataSource: new ListView.DataSource({
 				rowHasChanged: (row1, row2) => row1 !== row2,
-			})
+			}),
+			textInputValue: ''
 		};
 	}
 
-	// getRef() {
-	// 	return firebaseApp.database().ref();
-	// }
+	getRef() {
+		return firebaseApp.database().ref();
+	}
 
 	listenForCharters(chartersRef) {
 		chartersRef.on('value', (snap) => {
@@ -109,64 +136,99 @@ class SearchScreen extends React.Component {
 		this.listenForCharters(chartersRef);
 	}
 
-	render() {
-		// The screen's current route is passed in to `props.navigation.state`:
-		const { params } = this.props.navigation.state;
-		const { navigate } = this.props.navigation;
+	onPress() {
+		var value = this.refs.form.getValue();
+		var start = new Date(value.time);
+		chartersRef.orderByChild("destination").equalTo(value.destination).on("value", function(snap) {
+			snap.forEach(function(childSnap) {
+				var childData = childSnap.val();
 
-		return (
-			<Container backgroundColor='#EFF0F0'>
-			<Item floatingLabel style={{ width: '80%', marginLeft:'10%', marginTop:'15%', }}>
-			<Label>Search</Label>
-			<Input />
-			</Item>
-			<Content style={{ marginTop: '7.5%' }} >
-			<Grid>
-			<Row>
-			<ListView
-				dataSource={this.state.dataSource}
-				renderRow={(rowData) => <Text style={styles.welcome}>{rowData}</Text>}
-			/>
+				var qstart = new Date(childData.time);
+				if ((Math.abs(start - qstart) / 60000) < 30) {
+					// DO SOMETHING HERE
+				}
+			});
+		});
 
-			</Row>
-			</Grid>
-
-			</Content>
-			<Button raised title='CREATE NEW RIDE' style={{ float:'bottom' }} backgroundColor='#7CD3C8' onPress={() => navigate('Create', { user: 'Christopher Eisgruber' })}/>
-			</Container>
-		);
 	}
-}
 
-class ListScreen extends React.Component {
-	static navigationOptions = {
-		// Nav options can be defined as a function of the navigation prop:
-		title: ({ state }) => `Ride listings for ${state.params.user}`,
-	};
 	render() {
-		// The screen's current route is passed in to `props.navigation.state`:
+
 		const { params } = this.props.navigation.state;
 		const { navigate } = this.props.navigation;
+
+		var Destinations = t.enums({
+			'PEN': 'Penn Station NY',
+			'JFK': 'JFK Airport NY',
+			'PHL': 'Philadelphia Airport'
+		});
+
+		// here we are: define your domain model
+		var newCharter = t.struct({
+			destination: Destinations,     // a required string
+			time: t.Date               // a required number
+		});
+
+		var options = {
+			fields: {
+				destination: {
+					label: 'Where to?'
+				},
+				time: {
+					label: 'When?',
+					config: {
+						format: (date) => {
+							// TODO remove seconds
+							var d = String(date.toDateString() + ' ' + date.toLocaleTimeString());
+							return d.substring(0, d.length - 6).concat(d.substring(d.length - 3, d.length));
+						}
+					}
+				}
+			},
+
+		}; // optional rendering options (see documentation)
+
+		
 		return (
-			<View>
-			<Text>Show more...</Text>
-			<Button
-			onPress={() => navigate('Detail', { user: 'Christopher Eisgruber' })}
-			title="Detail > "
-			/>
-			<Button
-			onPress={() => navigate('Create', { user: 'Christopher Eisgruber' })}
-			title="Create New Ride >"
-			/>
+			<View style={styles.container}>
+				<Text style={{color:"white", fontSize:45, textAlign:"center", letterSpacing:2.5, fontWeight:'800', fontFamily:"Jaapokki subtract"}}>SEARCH</Text>
+				<NewCharterForm
+					ref="form"
+					type={newCharter}
+					options={options}
+				/>
+
+				<TouchableHighlight style={styles.button} onPress={this.onPress.bind(this)} underlayColor='#e2d662'>
+					<Text style={styles.buttonText}>SEARCH</Text>
+				</TouchableHighlight>
+				<TouchableHighlight style={styles.button} onPress={() => navigate('Create', { })} underlayColor='#e2d662'>
+					<Text style={styles.buttonText}>CREATE</Text>
+				</TouchableHighlight>
 			</View>
+			
+
 		);
 	}
 }
+
 
 class DetailScreen extends React.Component {
 	static navigationOptions = {
 		// Nav options can be defined as a function of the navigation prop:
-		title: ({ state }) => `Details for ride`,
+		title: ({ state }) => ` `,
+		header: {
+		    titleStyle: {
+		     	color: '#B5BABF',
+		     	letterSpacing: 2,
+		     	fontWeight: '500'
+		    },
+		    style: {
+		     	backgroundColor: '#092742'
+		    },
+		    tintColor: {
+		      	backgroundColor: '#FCEE6D'
+		    }
+		  }
 	};
 	render() {
 		// The screen's current route is passed in to `props.navigation.state`:
@@ -175,32 +237,32 @@ class DetailScreen extends React.Component {
 			/*<View>
 			<Text>Ride details</Text>
 			</View>*/
-			<Container>
-			<Content>
-			<Form>
-			<Item floatingLabel>
-			<Label>Departure Time:</Label>
-			<Input />
-			</Item>
-			<Item floatingLabel last>
-			<Label>Estimated Arrival Time:</Label>
-			<Input />
-			</Item>
-			<Item floatingLabel last>
-			<Label>Destination:</Label>
-			<Input />
-			</Item>
-			<Item floatingLabel last>
-			<Label>Pickup Location:</Label>
-			<Input />
-			</Item>
-			<Item floatingLabel last>
-			<Label>Current Riders:</Label>
-			<Input />
-			</Item>
-			</Form>
-			</Content>
-			</Container>
+			<View style={styles.container}>
+				<Content>
+					<Form>
+						<Item floatingLabel>
+							<Label>Departure Time:</Label>
+							<Input />
+						</Item>
+						<Item floatingLabel last>
+							<Label>Estimated Arrival Time:</Label>
+							<Input />
+						</Item>
+						<Item floatingLabel last>
+							<Label>Destination:</Label>
+							<Input />
+						</Item>
+						<Item floatingLabel last>
+							<Label>Pickup Location:</Label>
+							<Input />
+						</Item>
+						<Item floatingLabel last>
+							<Label>Current Riders:</Label>
+							<Input />
+						</Item>
+					</Form>
+				</Content>
+			</View>
 		);
 	}
 }
@@ -208,7 +270,20 @@ class DetailScreen extends React.Component {
 class CreateScreen extends React.Component {
 	static navigationOptions = {
 		// Nav options can be defined as a function of the navigation prop:
-		title: ({ state }) => `Create a new ride`
+		title: ({ state }) => " ",
+		header: {
+		    titleStyle: {
+		     	color: '#B5BABF',
+		     	letterSpacing: 2,
+		     	fontWeight: '500'
+		    },
+		    style: {
+		     	backgroundColor: '#092742'
+		    },
+		    tintColor: {
+		      	backgroundColor: '#FCEE6D'
+		    }
+		  }
 	};
 
 	// // TODO this is the logic for joining a charter
@@ -218,19 +293,24 @@ class CreateScreen extends React.Component {
 	// }
 
 	onPress() {
+		const { navigate } = this.props.navigation;
 		var value = this.refs.form.getValue();
 		let newCharter = chartersRef.push({
 			destination: value.destination,
 			owner: currentUser,
 			pickup: value.pickup,
 			time: value.time.toString(),
-			timeline: { m1: "Welcome to your new Charter! Use this timeline to post any updates." }
+			timeline: { m1: 'Welcome to your new Charter! Use this timeline to post any updates.' }
 		}).key;
 
 		firebaseApp.database().ref('users/' + currentUser + '/charters-owned/' + newCharter).set(true);
+		navigate('Detail', { });
 	}
 
 	render() {
+
+		const { params } = this.props.navigation.state;
+		const { navigate } = this.props.navigation;
 
 		var Destinations = t.enums({
 			'PEN': 'Penn Station NY',
@@ -254,10 +334,10 @@ class CreateScreen extends React.Component {
 		var options = {
 			fields: {
 				destination: {
-					label: 'Where to?'
+					label: "Where to?"
 				},
 				time: {
-					label: 'When?',
+					label: "When?",
 					config: {
 						format: (date) => {
 							// TODO remove seconds
@@ -266,17 +346,18 @@ class CreateScreen extends React.Component {
 					}
 				}
 			}
-		}; // optional rendering options (see documentation)
+		}; 
 
 		return (
 			<View style={styles.container}>
+				<Text style={{color:"white", fontSize:45, textAlign:"center", letterSpacing:2.5, fontWeight:'800', fontFamily:"Jaapokki subtract"}}>CREATE A RIDE</Text>
 				<NewCharterForm
 					ref="form"
 					type={newCharter}
 					options={options}
 				/>
-				<TouchableHighlight style={styles.button} onPress={this.onPress.bind(this)} underlayColor='#99d9f4'>
-					<Text style={styles.buttonText}>Save</Text>
+				<TouchableHighlight style={styles.button} onPress={this.onPress.bind(this)} underlayColor='#e2d662'>
+					<Text style={styles.buttonText}>SAVE</Text>
 				</TouchableHighlight>
 			</View>
 		);
@@ -288,8 +369,8 @@ const styles = StyleSheet.create({
 	container: {
 		justifyContent: 'center',
 		padding: 20,
-		backgroundColor: '#EFF0F0',
-		flex: 1
+		backgroundColor: '#0C3458',
+		flex: 1,
 	},
 	title: {
 		fontSize: 30,
@@ -297,33 +378,43 @@ const styles = StyleSheet.create({
 		marginBottom: 30
 	},
 	buttonText: {
-		fontSize: 18,
-		color: 'white',
-		alignSelf: 'center'
+		fontSize: 15,
+		color: '#2C3440',
+		//color: 'white',
+		alignSelf: 'center',
+		fontWeight: 'bold',
+		letterSpacing: 2,
 	},
 	button: {
 		height: 36,
-		backgroundColor: '#48BBEC',
-		borderColor: '#48BBEC',
+		//backgroundColor: '#48BBEC',
+		//borderColor: '#48BBEC',
+		backgroundColor: '#FCEE6D',
+		borderColor: '#FCEE6D',
 		borderWidth: 1,
 		borderRadius: 8,
 		marginBottom: 10,
 		alignSelf: 'stretch',
-		justifyContent: 'center'
+		justifyContent: 'center',
+
 	},
 	welcome: {
 		fontSize: 20,
 		textAlign: 'center',
 		margin: 10,
-	}
+	},
+
 });
 
 const Charter = StackNavigator({
 	Login:  { screen: LoginScreen  },
 	Search: { screen: SearchScreen },
-	List:   { screen: ListScreen   },
 	Detail: { screen: DetailScreen },
 	Create: { screen: CreateScreen },
+}, {
+      headerMode: 'screen'
 });
+
+
 
 AppRegistry.registerComponent('Charter', () => Charter);
